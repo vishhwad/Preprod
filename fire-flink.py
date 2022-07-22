52ce94239509fdcde36fb0369d0a080b79a52fd8
vishhwad <108934884+vishhwad@users.noreply.github.com>
2022-07-22 16:16:07 +0530

    Update fire-flink.py

--- fire-flink.py
+++ fire-flink.py
@@ -19,20 +19,20 @@ def login(token):
     }
     head["Authorization"] = "Bearer " + token
     suiteid = 'SUITE1035'
-    pes = s.post('https://app.flinko.com:8109/optimize/v1/dashboard/execution/suite/' + suiteid, headers=head)
+    pes = s.post('https://app.fireflink.com:8109/optimize/v1/dashboard/execution/suite/' + suiteid, headers=head)
     out = json.loads(pes.content)
     exid = out['responseObject']['id']
 
     time.sleep(3)
     sc = 0
     while (sc < 1):
-        r1 = s.get('https://app.flinko.com:8109/optimize/v1/dashboard/execution/' + exid, headers=head)
+        r1 = s.get('https://app.fireflink.com:8109/optimize/v1/dashboard/execution/' + exid, headers=head)
         c1 = json.loads(r1.content)
         fr1 = c1['responseObject']['executionStatus']
         print('status : ' + fr1 + '......')
         if (fr1 == "Completed" or fr1 == "Terminated" or fr1 == "Warning"):
             if fr1 == 'Completed':
-                r2 = s.get('https://app.flinko.com:8110/optimize/v1/executionResponse/result/' + exid, headers=head)
+                r2 = s.get('https://app.fireflink.com:8110/optimize/v1/executionResponse/result/' + exid, headers=head)
                 c2 = json.loads(r2.content)
                 fr2 = c2['responseObject']['suiteStatus']
                 if fr2 == 'FAIL':
